2af3b47d0040a30ade7c227b3552af3c0ab70305
morisum <moris@r.sannet.ne.jp>
2024-01-16 14:08:26 +0900

    AppleWatchのタイマーを作成した

--- Timer2 Watch App/ContentView.swift
+++ Timer2 Watch App/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
+    @State var timer: Timer?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(count)")
+                .padding()
+            Button(action: {
+                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){_ in
+                    self.count += 1
+                }
+            }){
+                Text("開始")
+            }
+            Button(action: {
+                if let unWrapedtimer = timer {
+//                    値があるか調べる必要があるか？
+                    if unWrapedtimer.isValid == true {
+                        unWrapedtimer.invalidate()
+                    }
+                }
+            }){
+                Text("ストップ")
+            }
+            Button(action: {
+                if let unWrapedtimer2 = timer {
+//                    ここでは調べてないが、エラー無しに動く
+                        unWrapedtimer2.invalidate()
+                        count = 0
+                }
+            }){
+                Text("reset")
+            }
         }
         .padding()
     }
