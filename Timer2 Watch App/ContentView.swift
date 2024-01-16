8ac15918aafb02c274a0f3eac96fd915dcd673b4
morisum <moris@r.sannet.ne.jp>
2024-01-16 11:08:00 +0900

    Initial Commit

---
+++ Timer2 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timer2 Watch App
+//
+//  Created by MsMacM on 2024/01/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timer2 Watch App/Timer2App.swift
@@ -0,0 +1,17 @@
+//
+//  Timer2App.swift
+//  Timer2 Watch App
+//
+//  Created by MsMacM on 2024/01/16.
+//
+
+import SwiftUI
+
+@main
+struct Timer2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
